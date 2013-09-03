ae418ec7dfe22d76351240a88ce5e5921604033c
y-ken <y.ken.studio@gmail.com>
2013-09-03 22:19:18 +0900

    get compatible behavior for current twimlbin.com #1

--- lib/twilio-twimlbin.rb
+++ lib/twilio-twimlbin.rb
@@ -13,22 +13,20 @@ class Twimlbin
   def configure
     @http = Net::HTTP.new('twimlbin.com', 80)
     res = @http.get('/create')
-    cookie = res.response['set-cookie'].split('; ')[0]
-    @header = { "Cookie" => cookie }
     @token = res.response['Location'].scan(/[^\/]+$/).last
     @private_url = "http://twimlbin.com/#{@token}"
     @external_url = get_external_url
   end
 
   def get_external_url
-    res = @http.get("/#{@token}", @header)
+    res = @http.get("/#{@token}")
     find_url_regex = Regexp.escape('http://twimlbin.com/external/')
     regex = Regexp.new("(#{find_url_regex}[^<]+)")
     return res.body[regex]
   end
 
   def publish(xml)
-    request = Net::HTTP::Post.new("/save_data", @header)
+    request = Net::HTTP::Post.new("/save_data")
     body = {'index' => @token, 'twiml' => xml}
     request.set_form_data(body)
     begin
@@ -37,6 +35,5 @@ class Twimlbin
       raise Gem::Exception, "Twimlbin: Error: #{e.message}"
     end
   end
-
 end
 
